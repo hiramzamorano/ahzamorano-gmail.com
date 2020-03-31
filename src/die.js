6994c656645e04be3772a525d89ad63ca20273a2
hiramzamorano <ahzamorano@gmail.com>
2020-03-30 18:46:50 -0700

    Initial commit

---
+++ src/die.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Die extends Component {
+    render() {
+        return (
+            <div>
+               <i className='fas fa-dice-one' />
+            </div>
+        );
+    }
+}
+
+export default Die;
